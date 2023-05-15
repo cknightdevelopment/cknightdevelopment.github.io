d3f80c9b2f49728f06789757f4c57e3e59e4a925
Chris Knight <cknightdevelopment@gmail.com>
2023-05-15 13:37:50 -0500

    test yes css nano

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...(process.env.JEKYLL_ENV == 'productionZ'
+    ...(process.env.JEKYLL_ENV == 'production'
       ? [require('cssnano')({ preset: 'default' })]
       : [])
   ]
